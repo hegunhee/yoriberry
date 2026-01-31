c7b1780d75eb5ba3149b2750a0c45296ddec3ea4
이건희 <leech9876@naver.com>
2026-01-31 23:06:51 +0900

    #3 - feat : 재료, 대체 재료 class 작성

---
+++ lib/features/purchase/models/ingredient.dart
@@ -0,0 +1,34 @@
+import 'package:template/features/purchase/models/substitute.dart';
+
+/// 재료 정보를 담는 클래스
+class Ingredient {
+
+  /// 재료 생성자
+  const Ingredient({
+    required this.name,
+    required this.substitutes,
+    required this.description,
+    required this.imageUrl,
+    required this.purchaseUrl,
+  });
+
+  /// 재료 이름
+  final String name;
+
+  /// 대체 가능한 재료 목록
+  final List<Substitute> substitutes;
+
+  /// 재료 설명
+  final String description;
+
+  /// 재료 이미지 URL
+  final String imageUrl;
+
+  /// 구매 링크 URL
+  final String purchaseUrl;
+
+  @override
+  String toString() {
+    return 'Ingredient(name: $name, substitutes: $substitutes, description: $description, imageUrl: $imageUrl, referenceUrl: $purchaseUrl)';
+  }
+}
